dfbe3466b02075671d4c6a55b8f0c185073bee05
pfwang <2411238759@qq.com>
2024-03-17 18:27:30 +0800

    修复各种场景下的逻辑bug

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,6 @@ impl<'a> SqlCommentParser<'a> {
                             break;
                         }
                     }
-                    break;
                 }
                 '`' => {
                     self.start = self.pos;
@@ -76,7 +75,6 @@ impl<'a> SqlCommentParser<'a> {
                             break;
                         }
                     }
-                    break;
                 }
                 '\"' => {
                     self.start = self.pos;
@@ -88,16 +86,15 @@ impl<'a> SqlCommentParser<'a> {
                             break;
                         }
                     }
-                    break;
                 }
                 '/' => {
                     // possible start of '/*'
                     if self.pos + 1 < self.sql.len() {
-                        let c = self.sql.as_bytes()[self.pos] as char;
+                        let c = self.sql.as_bytes()[self.pos + 1] as char;
                         if c == '*' {
                             start_index = self.pos;
                             // 从pos + 2开始查找"*/"
-                            let end: usize = match self.sql[self.pos + 2..].find("*/") {
+                            let end: usize = match self.sql.find("*/") {
                                 Some(end) => end + "*/".len(),
                                 None => self.sql.len(),
                             };
@@ -126,8 +123,8 @@ impl<'a> SqlCommentParser<'a> {
                     if SqlCommentParser::is_open_quote(c) {
                         break;
                     } else {
-                        self.pos += 1;
                         loop {
+                            self.pos += 1;
                             if self.pos >= self.sql.len() {
                                 break;
                             }
@@ -141,9 +138,8 @@ impl<'a> SqlCommentParser<'a> {
                                         break;
                                     }
                                 }
-                                _ => self.pos += 1,
+                                _ => {}
                             }
-                            self.pos += 1;
                         }
                     }
                 }
@@ -184,5 +180,22 @@ fn main() {
     let sql = "SELECT * FROM table--; -- This is a single line comment";
     let mut parser = SqlCommentParser::new(&sql);
     let cleaned_sql = parser.remove_comment_sql();
-    println!("{}", cleaned_sql)
+    println!("{}", cleaned_sql);
+
+    let sql_with_two_comments = "SELECT * FROM table--; -- Comment 1\n-- Comment 2";
+    let mut parser2 = SqlCommentParser::new(&sql_with_two_comments);
+    let cleaned_sql2 = parser2.remove_comment_sql();
+    println!("{}", cleaned_sql2);
+
+    let sql = "SELECT * FROM table /* This is a multiline
+                     comment that spans across multiple lines */ WHERE id = 1";
+    let mut parser = SqlCommentParser::new(&sql);
+    let cleaned_sql = parser.remove_comment_sql();
+    println!("{}", cleaned_sql);
+
+    let sql_with_quotes_and_comments =
+        r#"SELECT "col1" FROM `table` WHERE 'string' = 'value' /* Multiline comment */ AND id = 2"#;
+    let mut parser3 = SqlCommentParser::new(&sql_with_quotes_and_comments);
+    let cleaned_sql3 = parser3.remove_comment_sql();
+    println!("{}", cleaned_sql3);
 }
